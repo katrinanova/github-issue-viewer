8f8cbd11b56e280948ceaf3fbe139d11f2276a63
Katrina Nova <katrina.a.nova@gmail.com>
2016-07-26 21:38:53 -0400

    set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var webpack = require('webpack');
+
+
+module.exports = {
+  entry: "./app/app.js",
+  output: {
+    path: "./",
+    filename: "bundle.js"
+  }
+};
+
+
+module.exports = {
+    entry: "./app/app.js",
+    output: {
+        filename: "public/js/bundle.js",
+        sourceMapFilename: "public/js/bundle.map"
+    },
+    devtool: '#source-map',
+    module: {
+      loaders: [
+        {
+          test: [/\.jsx?$/, /\.js?$/],
+          exclude: /node_modules/,
+          loader: 'babel',
+          query: {
+            presets: ['react', 'es2015']
+          }
+        }
+      ]
+    },
+    resolve: {
+      extensions: ["", ".js", ".jsx"]
+    }
+}
