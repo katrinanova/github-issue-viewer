86262bc4ec60b26d5a82a1a11995254927befc13
Katrina Nova <katrina.a.nova@gmail.com>
2016-07-26 21:43:39 -0400

    set up server

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+app = express();
+
+app.use(express.static('public'));
+
+http = require('http');
+httpServer = http.Server(app);
+
+
+app.get('/*', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+var port = Number(process.env.PORT || 3000);
+app.listen(port);
